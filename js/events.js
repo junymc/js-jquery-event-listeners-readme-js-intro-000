51c9db45d9d12b551f7d7992f7b5a9fa4c9c435c
Junyoung McArdle <juny.mcardle@gmail.com>
2018-12-26 20:44:28 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -8,6 +8,14 @@ function frameIt() {
   $('img').on("load", function() {
     $('img').addClass("tasty")
   })
+}
+
+function pressIt() {
+  $('form').on("keydown", function(key) {
+    if(key.which == 71) {
+      alert('g was pressed')
+    }
+  })
 }//define functions here
 
 $(document).ready(function(){
