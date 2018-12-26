f1eeeb16c7522f2436373fb085e66d887e5d5faf
Junyoung McArdle <juny.mcardle@gmail.com>
2018-12-26 20:34:56 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,12 @@ function getIt() {
   $('p').on("click", function(){
     alert("Hey!")
   })
+}
+
+function framIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty")
+  })
 }//define functions here
 
 $(document).ready(function(){
